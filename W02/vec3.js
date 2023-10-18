ce45efd60140dec7b4d565ed1bd58501a56a8ee8
Okami Naoki <cocona2345cocona@gmail.com>
2023-10-18 22:51:29 +0900

    kadaiEnd

--- W02/vec3.js
+++ W02/vec3.js
@@ -51,12 +51,13 @@ function Vec3(x, y, z) {
   }
 
   function AreaOfTriangle( v0, v1, v2 ) {
-    return Math.sqrt(
-        (Math.pow(v0.x - v1.x, 2) + Math.pow(v0.y - v1.y, 2) + Math.pow(v0.z - v1.z, 2)) *
-        (Math.pow(v0.x - v2.x, 2) + Math.pow(v0.y - v2.y, 2) + Math.pow(v0.z - v2.z, 2)) -
-        (Math.pow(v0.x - v1.x, 2) + Math.pow(v0.y - v1.y, 2) + Math.pow(v0.z - v1.z, 2)) *
-        (Math.pow(v0.x - v2.x, 2) + Math.pow(v0.y - v2.y, 2) + Math.pow(v0.z - v2.z, 2))
-      ) / 2;
- }
+    // 3つの辺の長さを計算
+    var a = Math.sqrt(Math.pow(v1.x - v0.x, 2) + Math.pow(v1.y - v0.y, 2) + Math.pow(v1.z - v0.z, 2));
+    var b = Math.sqrt(Math.pow(v2.x - v1.x, 2) + Math.pow(v2.y - v1.y, 2) + Math.pow(v2.z - v1.z, 2));
+    var c = Math.sqrt(Math.pow(v0.x - v2.x, 2) + Math.pow(v0.y - v2.y, 2) + Math.pow(v0.z - v2.z, 2));
+    var s = (a + b + c) / 2;
+    var area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+    return area;
+}
 
   
